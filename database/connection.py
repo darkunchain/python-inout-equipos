5439d70cb9fca737bb7f032e64592b03e4f3469e
darkunchain <luisfer74081@gmail.com>
2026-05-04 22:16:37 -0500

    v1.0 linux

--- database/connection.py
+++ database/connection.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 import sqlite3
+import sys
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+def get_base_dir():
+    """
+    En desarrollo usa la carpeta raíz del proyecto.
+    En ejecutable usa la carpeta donde está el .exe.
+    """
+    if getattr(sys, "frozen", False):
+        return Path(sys.executable).resolve().parent
+
+    return Path(__file__).resolve().parent.parent
+
+
+BASE_DIR = get_base_dir()
 DATA_DIR = BASE_DIR / "data"
 DB_PATH = DATA_DIR / "control_equipos.db"
 
@@ -12,7 +24,6 @@ def get_connection():
 
     connection = sqlite3.connect(DB_PATH)
     connection.row_factory = sqlite3.Row
-
     connection.execute("PRAGMA foreign_keys = ON;")
 
     return connection
